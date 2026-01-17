837b8e22922cb9e4fdba72580597970f1ba02777
dustin-aeria <dustin@aeria.ai>
2026-01-16 16:05:42 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,15 @@ import Aircraft from './pages/Aircraft'
 import Clients from './pages/Clients'
 import Settings from './pages/Settings'
 
+// Safety Module Pages
+import SafetyDashboard from './pages/SafetyDashboard'
+import Incidents from './pages/Incidents'
+import IncidentReport from './pages/IncidentReport'
+import IncidentDetail from './pages/IncidentDetail'
+import Capas from './pages/Capas'
+import CapaNew from './pages/CapaNew'
+import CapaDetail from './pages/CapaDetail'
+
 // Protected route wrapper
 function ProtectedRoute({ children }) {
   const { isAuthenticated, loading } = useAuth()
@@ -82,6 +91,17 @@ function App() {
         <Route path="aircraft" element={<Aircraft />} />
         <Route path="clients" element={<Clients />} />
         <Route path="settings" element={<Settings />} />
+        
+        {/* Safety Module Routes */}
+        <Route path="safety" element={<SafetyDashboard />} />
+        <Route path="incidents" element={<Incidents />} />
+        <Route path="incidents/new" element={<IncidentReport />} />
+        <Route path="incidents/:id" element={<IncidentDetail />} />
+        <Route path="incidents/:id/edit" element={<IncidentReport />} />
+        <Route path="capas" element={<Capas />} />
+        <Route path="capas/new" element={<CapaNew />} />
+        <Route path="capas/:id" element={<CapaDetail />} />
+        <Route path="capas/:id/edit" element={<CapaNew />} />
       </Route>
       
       {/* Catch all - redirect to dashboard */}
